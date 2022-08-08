d92b480659f6783b2e3d4891f328d65393eed2fe
Johans <johanscedeno@gmail.com>
2022-08-08 19:21:10 -0300

    simpler version of arrow buttons

--- src/index.js
+++ src/index.js
@@ -13,62 +13,62 @@ import "./index.scss";
 const normalizedD100 = normalizeTeslaModelData(metrics100D);
 const normalizedP100D = normalizeTeslaModelData(metricsP100D);
 
-const model = initReactivity({
+const initialState = {
   kmh: 70,
   temp: -10,
   ac: "off",
-  wheelsize: 21,
+  wheelsize: 19,
   d100Km: 0,
   pD100Km: 0,
-});
-const updateDomInputs = () => {
-  const reactiveLessModel = { ...model };
-  Object.keys(reactiveLessModel).forEach((key) => {
-    const [input] = document.getElementsByName(key);
-    if (input && input.value !== reactiveLessModel[key]) {
-      input.value = reactiveLessModel[key];
-      input.checked = true;
-    }
-  });
 };
 
-// document
-//   .getElementById("battery-range-calculator")
-//   .addEventListener("change", ({ target: eventTarget }) => {
-//     const { name, value, checked, type } = eventTarget;
-//     const isCheckbox = type === "checkbox";
-//     const valueToUse = isCheckbox ? (checked && "on") || "off" : value;
+const model = initReactivity(initialState);
 
-//     model[name] = valueToUse;
-//   });
+Object.keys(initialState).forEach((key) => {
+  const [input] = document.getElementsByName(key);
+  if (input) {
+    input.checked = true;
+    input.value = initialState[key];
+  }
+});
 
-[...document.querySelectorAll(".arrow-controls")].forEach(elem => {
+document
+  .getElementById("battery-range-calculator")
+  .addEventListener("change", ({ target: eventTarget }) => {
+    const { name, value, checked, type } = eventTarget;
+    const isCheckbox = type === "checkbox";
+    const valueToUse = isCheckbox ? (checked && "on") || "off" : value;
+
+    model[name] = valueToUse;
+  });
+
+[...document.querySelectorAll(".arrow-controls")].forEach((elem) => {
   elem.addEventListener("click", ({ target: arrow }) => {
     const { name } = arrow;
+    const control = arrow.getAttribute("control");
     const input = document.getElementById(name);
-    const { step, min, max } = getAttributes(input, ["step", "min", "max"]);
-    const action = arrow.getAttribute("control");
-    const stepCalculated = (action === "up" ? 1 : -1) * step;
-
-    model[name] = clamp(stepCalculated + { ...model }[name], min, max);
+    if (control === "up") {
+      input.stepUp();
+    } else {
+      input.stepDown();
+    }
+    const change = new Event("change", { bubbles: true });
+    input.dispatchEvent(change);
   });
 });
 
 const renderFunction = () => {
   document.getElementById("100D-km").innerHTML = model.d100Km;
   document.getElementById("P100D-km").innerHTML = model.pD100Km;
-  updateDomInputs();
 };
 
 watchReactive(() => {
-  model.d100Km = calculateKm(normalizedD100, model);
-  model.pD100Km = calculateKm(normalizedP100D, model);
+  model.d100Km = calculateKm(normalizedD100, { ...model });
+  model.pD100Km = calculateKm(normalizedP100D, { ...model });
 });
 
 watchReactive(renderFunction);
 
 document
   .getElementById("print")
-  .addEventListener("click", () =>
-    console.log({ bla: document.querySelector(".tesla-controls--arrows") })
-  );
+  .addEventListener("click", () => console.log({ model }));
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -20,16 +20,7 @@ export const normalizeTeslaModelData = (data) => {
   }, {});
 };
 
-export const calculateKm = (teslaModel, data) =>
+export const calculateKm = (teslaModel, data) => 
   teslaModel.byWheelsize?.[data.wheelsize]?.byAc[data.ac]?.byTemp[
     data.temp
   ]?.find(({ kmh }) => data.kmh == kmh)?.kilometers ?? 0;
-
-export const getAttributes =  (elem, attributes) => {
-  return attributes.reduce((attrObj, attr) => {
-    attrObj[attr] = elem.getAttribute(attr);
-    return attrObj;
-  }, {})
-}
-
-export const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
