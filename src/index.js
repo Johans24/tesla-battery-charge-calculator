d924a15a98fe640e821c2b066ab444e3b0693f04
Johans <johanscedeno@gmail.com>
2022-08-09 00:00:24 -0300

    tesla settings and tesla fan

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import metrics100D from "./data/metric-100D.json";
 import metricsP100D from "./data/metric-P100D.json";
-import {
-  normalizeTeslaModelData,
-  calculateKm,
-  getAttributes,
-  clamp,
-} from "./utils/utils";
+import { normalizeTeslaModelData, calculateKm } from "./utils/utils";
 import { initReactivity, watchReactive } from "./utils/reactivity";
 
 import "./index.scss";
@@ -37,7 +32,7 @@ document
   .addEventListener("change", ({ target: eventTarget }) => {
     const { name, value, checked, type } = eventTarget;
     const isCheckbox = type === "checkbox";
-    const valueToUse = isCheckbox ? (checked && "on") || "off" : value;
+    const valueToUse = isCheckbox ? checked && "on" || "off" : value;
 
     model[name] = valueToUse;
   });
@@ -57,9 +52,38 @@ document
   });
 });
 
+document.getElementById("tesla-fan").addEventListener("click", () => {
+  const checkbox = document.getElementById("ac");
+  checkbox.checked = !checkbox.checked;
+  const change = new Event("change", { bubbles: true });
+  checkbox.dispatchEvent(change);
+});
+
 const renderFunction = () => {
   document.getElementById("100D-km").innerHTML = model.d100Km;
   document.getElementById("P100D-km").innerHTML = model.pD100Km;
+  const acLabel = document.getElementById("ac-label");
+  const acFan = document.getElementById("tesla-fan");
+  const acCheckbox = document.getElementById("ac");
+  const modelAc = model.ac;
+  const acOn = modelAc === "on";
+  const tempMinus10 = model.temp <= 10;
+
+  acCheckbox.checked = acOn;
+  acLabel.innerHTML = `${tempMinus10 ? 'heat' : 'ac'} ${modelAc}`;
+  if (acOn) {
+    acFan.classList.add("tesla-fan--active");
+  } else {
+    acFan.classList.remove("tesla-fan--active");
+  }
+  // acOn && acFan.classList.toggle("tesla-fan--active");
+
+  if (tempMinus10) {
+    acFan.classList.add("tesla-heat");
+  } else {
+    acFan.classList.remove("tesla-heat");
+  }
+  // tempMinus10 && acFan.classList.toggle("tesla-heat")
 };
 
 watchReactive(() => {
